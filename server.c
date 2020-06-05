db6d54d1268c356677f26cb8cb07c74a57a279da
David Horsley <david.e.horsley@gmail.com>
2020-06-05 17:00:25 +1000

    better errors and new web location

--- server.c
+++ server.c
@@ -13,10 +13,12 @@
 #define REP_URL "ws://localhost:8888/socks/rep"
 #define URL "http://localhost:8888/"
 
-void fatal(const char *what, int rv) {
-	fprintf(stderr, "%s: %s\n", what, nng_strerror(rv));
-	exit(1);
-}
+#define fatal(msg, s)                                                                              \
+	do {                                                                                       \
+		fprintf(stderr, "%s:%d (%s) error %s: %s\n", __FILE__, __LINE__, __FUNCTION__,     \
+		        msg, s);                                                                   \
+		exit(1);                                                                           \
+	} while (0)
 
 void inproc_server(void *arg) {
     (void) arg;
@@ -24,21 +26,24 @@ void inproc_server(void *arg) {
 	int rv;
 
 	if ((rv = buffered_stream_open(&s) != 0))
-		fatal("unable to set up inproc", rv);
+		fatal("unable to set up inproc", nng_strerror(rv));
 
 
     if ((rv = buffered_stream_listen(s, PUB_URL, REP_URL) != 0))
-		fatal("unable listen", rv);
+		fatal("unable listen", nng_strerror(rv));
 
 	char buf[254] = {};
 	for (uint32_t i = 0;; i++) {
         size_t n = snprintf(buf, sizeof(buf), "%d\n", i);
-        if ((rv=(buffered_stream_send(s, buf, n)) < 0)) fatal("send", rv);
+        if ((rv=(buffered_stream_send(s, buf, n)) < 0)) fatal("send", nng_strerror(rv));
 		nng_msleep(1000);
 	}
 }
 
 int main(int argc, char **argv) {
+    (void)argc;
+    (void)argv;
+
 	int rv;
 	nng_thread *inproc_thr;
 
@@ -47,42 +52,36 @@ int main(int argc, char **argv) {
 	nng_url *url;
 
 	if ((rv = nng_url_parse(&url, URL)) != 0) {
-		fatal("nng_url_parse", rv);
+		fatal("nng_url_parse", nng_strerror(rv));
 	}
 
 	printf("listening on %s\n", URL);
 	// Get a suitable HTTP server instance.  This creates one
 	// if it doesn't already exist.
 	if ((rv = nng_http_server_hold(&server, url)) != 0) {
-		fatal("nng_http_server_hold", rv);
+		fatal("nng_http_server_hold", nng_strerror(rv));
 	}
 
-	rv = nng_http_handler_alloc_file(&handler, "/", "./web/index.html");
+	rv = nng_http_handler_alloc_directory(&handler, "/", "./web/dist/");
 	if (rv != 0) {
-		fatal("nng_http_handler_alloc", rv);
+		fatal("nng_http_handler_alloc", nng_strerror(rv));
+	}
+	if ((rv = (nng_http_handler_set_tree(handler)) != 0)) {
+		fatal("nng_http_handler_add_handler", nng_strerror(rv));
 	}
 	if ((rv = nng_http_server_add_handler(server, handler)) != 0) {
-		fatal("nng_http_handler_add_handler", rv);
-	}
-
-	rv = nng_http_handler_alloc_directory(&handler, "/node_modules", "./web/node_modules/");
-	if (rv != 0) {
-		fatal("nng_http_handler_alloc", rv);
-	}
-
-	if ((rv = nng_http_server_add_handler(server, handler)) != 0) {
-		fatal("nng_http_handler_add_handler", rv);
+		fatal("nng_http_handler_add_handler", nng_strerror(rv));
 	}
 
 	if ((rv = nng_http_server_start(server)) != 0) {
-		fatal("nng_http_server_start", rv);
+		fatal("nng_http_server_start", nng_strerror(rv));
 	}
 
 	nng_url_free(url);
 
 	rv = nng_thread_create(&inproc_thr, inproc_server, NULL);
 	if (rv != 0) {
-		fatal("cannot start inproc server", rv);
+		fatal("cannot start inproc server", nng_strerror(rv));
 	}
 
 	// This runs forever.  The inproc_thr never exits, so we
